40853cd1da15d7d4277d534e07e8c1cece5e0375
Gregory Bove <gregbove101@gmail.com>
2024-04-01 22:30:49 -0400

    Fixing bash script to run all tests

--- src/tests/scripts/run_all_tests.sh
+++ src/tests/scripts/run_all_tests.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 # Get the parent directory (src)
-BASE_DIR=$(dirname "$(dirname "$(cd "$(dirname "$0")" && pwd)")")
+TESTS_DIR=$(dirname "$(cd "$(dirname "$0")" && pwd)")
 
-# Set PYTHONPATH to point to src/Generator directory
-export PYTHONPATH="$BASE_DIR/generator"
-
-# Run all test files in the Tests folder
-for test_file in "$BASE_DIR/tests"/*.py; do
+# Run all test files in the unit-tests folder
+for test_file in "$TESTS_DIR/unit-tests"/*.py; do
     if [ -f "$test_file" ]; then
         # echo "Running tests in $test_file"
         pytest "$test_file"
