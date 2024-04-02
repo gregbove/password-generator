b7689cca214aba1edc650b567254175e35c28b79
Gregory Bove <gregbove101@gmail.com>
2024-04-01 23:58:35 -0400

    Adding script to be added to .zshrc

---
+++ src/generator/scripts/main.sh
@@ -0,0 +1,12 @@
+# TODO tab completition?
+my-password-please() {
+    if [ "$#" -ne 2 ]; then
+        echo "Error: Please provide arguments."
+        echo "Usage: $0 <platform> <universal password>"
+        return 1
+    fi
+
+    echo "Generating your password for platform: $1"
+
+    python3 "$HOME/vs/password-generator/src/generator/main.py" $1 $2
+}
