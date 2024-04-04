94b1c3f0e27c59db5aa2449cf8c743eaf31f7f2c
Gregory Bove <gregbove101@gmail.com>
2024-04-04 01:21:55 -0400

    Update to prompt user for password

--- src/generator/main.py
+++ src/generator/main.py
@@ -1,6 +1,7 @@
 import argparse
 import pyperclip
-from engine import generate_password
+from auth import get_universal_password
+from engine import generate_platform_password
 
 def main():
     """
@@ -8,15 +9,16 @@ def main():
     """
 
     parser = argparse.ArgumentParser(description='Generate hashed passwords for different platforms.')
-    parser.add_argument('universal_password', type=str, help='The Universal Password')
     parser.add_argument('platform_name', type=str, help='Platform name')
     
     args = parser.parse_args()
-    
-    password = generate_password(args.universal_password, args.platform_name)
+
+    universal_password = get_universal_password()
+
+    platform_password = generate_platform_password(universal_password, args.platform_name)
 
     # Copy the password to the clipboard
-    pyperclip.copy(password)
+    pyperclip.copy(platform_password)
     print("Hashed password copied to clipboard.")
 
 if __name__ == "__main__":
